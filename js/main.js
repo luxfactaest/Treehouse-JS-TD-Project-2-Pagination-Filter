0ceaccd75a0a2cc58f5ab6805a1fc1a0ef3edba3
Adam Kelm <luxfactaest@gmail.com>
2017-04-20 14:11:50 -0400

    Changed tabs to spaces.
    And removed example files.

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ $(".page").append("<div class='pagination'><ul></ul></div>");
 
 // For each group of students, show a numbered page link to the bottom of the page
 for (var i = 1; i <= $totalStudents / perPageStudents + 1; i++) {
-	$(".pagination ul").append("<li><a href='#'>" + i + "</a></li>");
+    $(".pagination ul").append("<li><a href='#'>" + i + "</a></li>");
 }
 
 // Initialize the page with page 1 active
@@ -20,12 +20,12 @@ $(".pagination ul li a:first").addClass("active");
 
 // When a user clicks a pagination link, the previous link becomes inactive and the clicked link becomes active
 $(".pagination ul li a").on('click', function () {
-	$(".pagination ul li a").removeClass();
-	$(this).addClass("active");
+    $(".pagination ul li a").removeClass();
+    $(this).addClass("active");
 });
 
 // When a user clicks a pagination link, the page displays the corresponding page of students
 $(".pagination ul li").on('click', function() {
-	$(".student-list").children().hide();
-	$(".student-list").children().slice($(this).index() * perPageStudents, $(this).index() * perPageStudents + perPageStudents).show();
+    $(".student-list").children().hide();
+    $(".student-list").children().slice($(this).index() * perPageStudents, $(this).index() * perPageStudents + perPageStudents).show();
 });
